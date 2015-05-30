b3fffb62f4ab1239f7a6119a6d90bb7e21f06dab
jediguy13 <jediguy9@gmail.com>
2015-05-30 13:26:01 -0400

    Create template.js

---
+++ template.js
@@ -0,0 +1,11 @@
+var template = "";
+
+function replacePage(){
+  var heading = $("#heading").innerHTML;
+  var pghead = $("#pghead").innerHTML;
+  var pgtext = $("#pgtext").innerHTML;
+  $.get("/template.html", function(data){template=data;});
+  var sptemp = template.split("{{}}");
+  document.write(sptemp[0] + heading + sptemp[1] + pghead + sptemp[2] + pgtext + sptemp[3]);
+}
+  

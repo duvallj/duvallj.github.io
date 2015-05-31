f3cfa58ec5e96b88a162afffc46ecd82e33b598d
jediguy13 <jediguy9@gmail.com>
2015-05-31 12:45:09 -0400

    Update template.js

--- template.js
+++ template.js
@@ -1,24 +1,24 @@
-function replacePage(){
-  var heading = document.getElementById("heading").innerHTML;
-  var pghead = document.getElementById("pghead").innerHTML;
-  var pgtext = document.getElementById("pgtext").innerHTML;
-  var template = function () {
-    var tmp = null;
-    $.ajax({
-        'async': false,
-        'type': "POST",
-        'global': false,
-        'dataType': 'html',
-        'url': "/template.html",
-        'data': { 'request': "", 'target': 'arrange_url', 'method': 'method_target' },
-        'success': function (data) {
-            tmp = data;
-        }
-    });
-    return tmp;
+var heading = document.getElementById("heading").innerHTML;
+var pghead = document.getElementById("pghead").innerHTML;
+var pgtext = document.getElementById("pgtext").innerHTML;
+console.log(heading);
+console.log(pghead);
+console.log(pgtext);
+var template = function () {
+  var tmp = null;
+  $.ajax({
+      'async': false,
+      'type': "POST",
+      'global': false,
+      'dataType': 'html',
+      'url': "/template.html",
+      'data': { 'request': "", 'target': 'arrange_url', 'method': 'method_target' },
+      'success': function (data) {
+          tmp = data;
+      }
+  });
+  return tmp;
 }();
-    
-  var sptemp = template.split("derp");
-  document.write(sptemp[0] + heading + sptemp[1] + pghead + sptemp[2] + pgtext + sptemp[3]);
-}
-replacePage();
+console.log(template);
+ 
+var sptemp = template.split("derp");

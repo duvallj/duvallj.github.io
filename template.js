63504c534814602fab0bf5c7e35831057ec7a6f3
jediguy13 <jediguy9@gmail.com>
2015-05-31 08:49:24 -0400

    Improve Ajax

--- template.js
+++ template.js
@@ -1,11 +1,24 @@
-var template = "";
-
 function replacePage(){
-  var heading = $("#heading").innerHTML;
-  var pghead = $("#pghead").innerHTML;
-  var pgtext = $("#pgtext").innerHTML;
-  $.get("/template.html", function(data){template=data;});
-  var sptemp = template.split("{{}}");
+  var heading = document.getElementById("heading").innerHTML;
+  var pghead = document.getElementById("pghead").innerHTML;
+  var pgtext = document.getElementById("pgtext").innerHTML;
+  var template = function () {
+    var tmp = null;
+    $.ajax({
+        'async': false,
+        'type': "POST",
+        'global': false,
+        'dataType': 'html',
+        'url': "/template.html",
+        'data': { 'request': "", 'target': 'arrange_url', 'method': 'method_target' },
+        'success': function (data) {
+            tmp = data;
+        }
+    });
+    return tmp;
+}();
+    
+  var sptemp = template.split("derp");
   document.write(sptemp[0] + heading + sptemp[1] + pghead + sptemp[2] + pgtext + sptemp[3]);
 }
 replacePage();

5e79da99313464b055ca6d22ebffe4a6dffe9cb9
Jack <jediguy9@gmail.com>
2016-12-27 15:57:20 -0500

    Do more javascript
    Redo for more modularability, start support for sockets

---
+++ js/resize.js
@@ -0,0 +1,49 @@
+function RCanvas(canvasObj, rWidth, rHeight){
+  this.canvas = canvasObj;
+  this.ctx = this.canvas.getContext('2d');
+  this.rWidth = rWidth;
+  this.rHeight = rHeight;
+  this.wFactor = this.canvas.width / this.rWidth;
+  this.hFactor = this.canvas.height / this.rHeight;
+  this.objects = [];
+  this.draw = function(){
+    for(var obj in this.objects){
+      this.objects[obj].draw(this.ctx,this.wFactor,this.hFactor);
+    }
+  };
+  this.resize = function(){
+    this.wFactor = this.canvas.width / this.rWidth;
+    this.hFactor = this.canvas.height / this.rHeight;
+    this.draw();
+  };
+  this.add = function(obj){
+    this.objects[this.objects.length] = obj;
+  };
+}
+
+function RRect(x,y,width,height,fill){
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.fill = fill;
+  this.draw = function(ctx, wFactor, hFactor){
+    ctx.fillStyle = this.fill;
+    ctx.fillRect(this.x*wFactor, this.y*hFactor, this.width*wFactor, this.height*hFactor);
+  };
+}
+
+function RText(x,y,text,size,font,fill){
+  this.x = x;
+  this.y = y;
+  this.text = text;
+  this.size = size;
+  this.font = font;
+  this.fill = fill;
+  this.draw = function(ctx,wFactor,hFactor){
+    ctx.fillStyle = this.fill;
+    ctx.font = this.font;
+    ctx.fillText(Math.floor((hFactor+wFactor)/2*size).toString()+'px '+this.font, this.text,
+    this.x*wFactor,this.y*hFactor);
+  };
+}
--- js/reversi.js
+++ js/reversi.js
@@ -21,32 +21,39 @@ NM2CO[EMPTY_NM] = EMPTY_CO;
 NM2CO[WHITE_NM] = WHITE_CO;
 NM2CO[BLACK_NM] = BLACK_CO;
 
-function drawBoard(ctx, cWidth, cHeight, bSize, bArray){
-  var bdWidth = cWidth/(11*bSize+1); //from sq*s+bd*(s+1)=w, sq=10*bd
-  var bdHeight = cHeight/(11*bSize+1);
-  var sqWidth = 10*bdWidth;
-  var sqHeight = 10*bdHeight;
-  var uWidth = bdWidth + sqWidth;
-  var uHeight = bdHeight + sqHeight;
+function drawBoard(rCanvas, bSize, bArray){
+  if(bSize === rCanvas.bSize) {
+    for(var i=0; i<bSize*bSize; i++){
+      rCanvas.board[i].fill = NM2CO[bArray[i]];
+    }
+  } else {
+    var bdWidth = rCanvas.rWidth/(11*bSize+1); //from sq*s+bd*(s+1)=w, sq=10*bd
+    var bdHeight = rCanvas.rHeight/(11*bSize+1);
+    var sqWidth = 10*bdWidth;
+    var sqHeight = 10*bdHeight;
+    var uWidth = bdWidth + sqWidth;
+    var uHeight = bdHeight + sqHeight;
 
-  ctx.fillStyle = EMPTY_CO;
-  ctx.fillRect(0, 0, cWidth, cHeight);
+    rCanvas.objects = [];
 
-  ctx.fillStyle = BORDER_CO;
-  for(var i=0; i<=bSize; i++){
-    ctx.fillRect(uWidth*i, 0, bdWidth, cHeight);
-    ctx.fillRect(0, uHeight*i, cWidth, bdHeight);
-  }
+    for(var i=0; i<=bSize; i++){
+      rCanvas.add(new RRect(uWidth*i, 0, bdWidth, rCanvas.rHeight,BORDER_CO));
+      rCanvas.add(new RRect(0, uHeight*i,rCanvas.rWidth, bdHeight,BORDER_CO));
+    }
 
-  for(var y=0; y<bSize; y++){
-    for(var x=0; x<bSize; x++){
-      var check = bArray[bSize*y+x];
-      if(check !== EMPTY_NM){
-        ctx.fillStyle = NM2CO[check];
-        ctx.fillRect(uWidth*x+bdWidth, uHeight*y+bdHeight, sqWidth, sqHeight);
+    rCanvas.board = [];
+
+    for(var y=0; y<bSize; y++){
+      for(var x=0; x<bSize; x++){
+        var index = bSize*y+x;
+        var toAdd = new RRect(uWidth*x+bdWidth, uHeight*y+bdHeight, sqWidth, sqHeight, NM2CO[bArray[index]])
+        rCanvas.add(toAdd);
+        rCanvas.board[index] = toAdd;
       }
     }
   }
+  // add code to show player scores as well
+  rCanvas.draw();
 }
 
 function bStringToBArray(bString){
@@ -58,19 +65,36 @@ function bStringToBArray(bString){
 }
 
 function resize(canvas){
+  var oWidth = canvas.width;
   canvas.width = canvas.parentNode.offsetWidth*9/10;
-  canvas.height = canvas.width;
+  canvas.height = canvas.height * canvas.width / oWidth;
 }
 
-function draw(canvas){
-  resize(canvas);
-  var ctx = canvas.getContext('2d');
-  drawBoard(ctx,canvas.width,canvas.height,3,[0,1,2,1,2,0,2,0,1]);
-}
-
-function init(){
+function init(socket, delay){
+  document.getElementById('canvasContainer').innerHTML =
+    '<canvas id="canvas" width="400" height="400"></canvas>';
   var canvas = document.getElementById('canvas');
-  window.addEventListener('resize', function(){ draw(canvas); })
+  resize(canvas);
+  var rCanvas = new RCanvas(canvas, 890, 890);
+  window.addEventListener('resize', function(){ resize(canvas); rCanvas.resize(); });
+  drawBoard(rCanvas,5,[1,1,0,1,1,1,2,0,2,1,0,0,0,0,0,2,0,0,0,2,0,2,2,2,0]);
+  rCanvas.resize();
+  socket.on('reply', function(data){
+    if(data.type === 'board'){
+      drawBoard(rCanvas, data.bSize, bStringtoBArray(data.board));
+    }
+    elif(data.type === 'playerSwitch'){
+      //switch player turn
+    }
+    elif(data.type === 'playerWin'){
+      //display which player won
+    }
+  });
+  window.setInterval(function(){socket.emit('refresh',{});}, delay);
 }
-init();
-draw(document.getElementById('canvas'));
+
+function makeSocketFromPage(){
+  var socket = "derp";//io('http://'+document.getElementById('addr').value+':'+document.getElementById('port').value);
+  var delay = parseInt(document.getElementById('delay').value);
+  init(socket, delay);
+}
